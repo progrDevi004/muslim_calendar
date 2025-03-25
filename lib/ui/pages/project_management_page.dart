346367fbc80db71b585a72f684622ebde2a1baae
alici <alici.eyyub@gmail.com>
2025-03-25 09:45:09 +0100

    Projektmanagement Page korrigiert

--- lib/ui/pages/project_management_page.dart
+++ lib/ui/pages/project_management_page.dart
@@ -1,59 +1,3 @@
-<<<<<<< HEAD
-import 'package:Taqvimi/ui/components/platform_adaptive_scaffold_fab.dart';
-
-class ProjectManagementPage extends StatefulWidget {
-  // ... (existing code)
-}
-
-class _ProjectManagementPageState extends State<ProjectManagementPage> {
-  // ... (existing code)
-
-  @override
-  Widget build(BuildContext context) {
-    // ... (existing code)
-
-    return Scaffold(
-      // ... (existing code)
-
-      floatingActionButton: Platform.isIOS
-          ? Positioned(
-              right: 16,
-              bottom: 16,
-              child: CupertinoButton(
-                padding: EdgeInsets.zero,
-                onPressed: _showAddProjectDialog,
-                child: Container(
-                  width: 60,
-                  height: 60,
-                  decoration: BoxDecoration(
-                    color: CupertinoTheme.of(context).primaryColor,
-                    shape: BoxShape.circle,
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.2),
-                        blurRadius: 8,
-                        offset: const Offset(0, 2),
-                      ),
-                    ],
-                  ),
-                  child: const Icon(
-                    CupertinoIcons.add,
-                    color: CupertinoColors.white,
-                    size: 30,
-                  ),
-                ),
-              ),
-            )
-          : FloatingActionButton(
-              onPressed: _showAddProjectDialog,
-              backgroundColor: Theme.of(context).primaryColor,
-              child: const Icon(Icons.add),
-            ),
-    );
-  }
-
-  // ... (rest of the existing code)
-=======
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
@@ -1971,5 +1915,4 @@ class _ProjectManagementPageState extends State<ProjectManagementPage> {
       },
     );
   }
->>>>>>> Projektmanagement
 }
